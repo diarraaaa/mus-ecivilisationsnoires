eede0f2bd3ba7a843a406037d8172a1ba1b80314
diarraaaa <diarradieng062004@gmail.com>
2025-10-01 18:56:01 +0000

    mise en place des pages

--- python/routes.py
+++ python/routes.py
@@ -5,5 +5,8 @@ from python import app
 def index():
     return render_template('accueil.html')
 
+@app.route('/niveaupage')
+def pageniveau():
+    return render_template("niveau.html")
 if __name__ == '__main__':
     app.run(debug=True)
